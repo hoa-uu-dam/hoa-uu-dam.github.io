b775f37d3d2d50a7ffcf2e7a71ac2e2fd69ca623
Long Tran <mail@tranhl.me>
2026-05-05 13:51:03 +1000

    Use absolute base path

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
